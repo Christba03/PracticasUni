ba26bc413fd1edbfc7fc599c97299063249f3907
Christba03 <christba455@gmail.com>
2024-06-11 09:51:49 -0600

    updadte

---
+++ Javascript/concat.js
@@ -0,0 +1,4 @@
+let concat =firstName.concat(' ').concat(lastName);
+console.log(concat);
+let concat2=firstName + " " + lastName;
+console.log(concat2);
---
+++ Javascript/includes.js
@@ -0,0 +1,6 @@
+let tidsm = "Tecnlogias de la Informacion area Desarrollo de Software Mutliplataforma";
+let isMultimedia=tidsm.includes('Multimedia');
+console.log(isMultimedia);
+let isSoftware=tidsm.includes('Software');
+console.log(isSoftware);
+console.log(tidsm.toLowerCase());
